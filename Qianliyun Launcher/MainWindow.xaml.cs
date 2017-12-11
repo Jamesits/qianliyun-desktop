daba4b503273d72d12aaaf2bbe3ede87dc1255ed
Bingxing Wang <i@imbushuo.net>
2017-12-10 19:09:00 -0800

    Implement initial TarBar image button.

--- Qianliyun Launcher/MainWindow.xaml.cs
+++ Qianliyun Launcher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,23 @@ using System.Windows.Shapes;
 
 namespace Qianliyun_Launcher
 {
+
+    public partial class ImageButton : Button
+    {
+
+        [Description("The image displayed in the button if there is an Image control in the template " +
+            "whose Source property is template-bound to the ImageButton Source property."), Category("Common Properties")]
+        public ImageSource Source
+        {
+            get { return base.GetValue(SourceProperty) as ImageSource; }
+            set { base.SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(ImageSource), typeof(ImageButton));
+
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -25,4 +43,5 @@ namespace Qianliyun_Launcher
             InitializeComponent();
         }
     }
+
 }
