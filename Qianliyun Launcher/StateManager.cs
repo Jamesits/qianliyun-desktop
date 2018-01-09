e521fbc5e8f8d33ad97e1a20c672fa1706195864
James Swineson <github@public.swineson.me>
2018-01-09 23:03:59 +0800

    Add ObservableDictionary

--- Qianliyun Launcher/StateManager.cs
+++ Qianliyun Launcher/StateManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Net;
 using System.Net.Http;
 using System.Reflection;
 using System.Runtime.CompilerServices;
